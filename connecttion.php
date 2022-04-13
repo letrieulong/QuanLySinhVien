6c6aa467ecdf6a0eb0ab41ac170199fc5f2625b1
letrieulong <letrieulong412000@gmail.com>
2022-04-13 17:30:13 +0700

    dá

--- connecttion.php
+++ connecttion.php
@@ -1,8 +1,8 @@
 <?php
     //open connection to mysql db
-    $connection = mysqli_connect("us-cdbr-east-05.cleardb.net","be5e9c603b0178","8bbc452e","heroku_56f9eba53eaf186") or die("Error " . mysqli_error($connection));
+    // $connection = mysqli_connect("us-cdbr-east-05.cleardb.net","be5e9c603b0178","8bbc452e","heroku_56f9eba53eaf186") or die("Error " . mysqli_error($connection));
 
- // $connection = mysqli_connect("localhost","root","admin","connect") or die("Error " . mysqli_error($connection));
+ $connection = mysqli_connect("localhost","root","admin","connect") or die("Error " . mysqli_error($connection));
 
     //fetch table rows from mysql db
     $sql = "SELECT * FROM sinhvien";
@@ -11,11 +11,14 @@
 
     //create an array
     $emparray = array();
+
+    $rows = mysqli_fetch_assoc($result);
+
     while($row = mysqli_fetch_assoc($result))
     {
         array_push($emparray, $row);
     }
-    echo json_encode($emparray)."thanh cong";
+    echo  json_encode($emparray);
     //close the db connection
     mysqli_close($connection);
 ?>
