8ed00bf7c2ab05472f67e595311f103e39d5e819
letrieulong <letrieulong412000@gmail.com>
2022-04-13 15:26:04 +0700

    hgh

--- connecttion.php
+++ connecttion.php
@@ -2,13 +2,16 @@
     //open connection to mysql db
     $connection = mysqli_connect("us-cdbr-east-05.cleardb.net","be5e9c603b0178","8bbc452e","heroku_56f9eba53eaf186") or die("Error " . mysqli_error($connection));
 
+ // $connection = mysqli_connect("localhost","root","admin","connect") or die("Error " . mysqli_error($connection));
+
     //fetch table rows from mysql db
-    $sql = "select * from quanlysinhvien";
+    $sql = "SELECT * FROM lop";
+
     $result = mysqli_query($connection, $sql) or die("Error in Selecting " . mysqli_error($connection));
 
     //create an array
     $emparray = array();
-    while($row =mysqli_fetch_assoc($result))
+    while($row = mysqli_fetch_assoc($result))
     {
         array_push($emparray, $row);
     }
@@ -16,3 +19,6 @@
     //close the db connection
     mysqli_close($connection);
 ?>
+
+
+
