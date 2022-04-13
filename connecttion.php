c3f2aab4f35cf00ed668c74047ac27e0f8b25601
letrieulong <letrieulong412000@gmail.com>
2022-04-13 14:53:07 +0700

    h

--- connecttion.php
+++ connecttion.php
@@ -1,4 +1,11 @@
 <?php 
+$conn = mysqli_connect("localhost", "root", "admin", "connect") or die("Kết nối thất bại");
+$sql = "SELECT * FROM db";
+$result = mysqli_query($conn, $sql);
+$arr = array();
+while ($rows = mysqli_fetch_assoc($result)) {
+	array_push($arr, $rows);
+}
 echo "hellomanisdas";
 ?>
 
