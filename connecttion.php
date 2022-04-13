1163c5d6c95fa16b03c7a86b1da4f1846521a3f3
letrieulong <letrieulong412000@gmail.com>
2022-04-13 21:00:49 +0700

    da

--- connecttion.php
+++ connecttion.php
@@ -1,8 +1,8 @@
 <?php
     //open connection to mysql db
-    // $connection = mysqli_connect("us-cdbr-east-05.cleardb.net","be5e9c603b0178","8bbc452e","heroku_56f9eba53eaf186") or die("Error " . mysqli_error($connection));
+    $connection = mysqli_connect("us-cdbr-east-05.cleardb.net","be5e9c603b0178","8bbc452e","heroku_56f9eba53eaf186") or die("Error " . mysqli_error($connection));
 
- $connection = mysqli_connect("localhost","root","admin","connect") or die("Error " . mysqli_error($connection));
+ // $connection = mysqli_connect("localhost","root","admin","connect") or die("Error " . mysqli_error($connection));
 
     //fetch table rows from mysql db
     $sql = "SELECT * FROM lop";
@@ -18,7 +18,7 @@
     {
         array_push($emparray, $row);
     }
-    echo  json_encode($emparray);
+    echo json_encode($emparray);
     //close the db connection
     mysqli_close($connection);
 ?>
