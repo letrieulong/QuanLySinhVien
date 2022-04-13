ab4dca9f30c3063a79dc67f10b3bdc2593c6e690
letrieulong <letrieulong412000@gmail.com>
2022-04-13 11:28:58 +0700

    ok

---
+++ connect.php
@@ -0,0 +1,17 @@
+<?php 
+// kết nối tới mysql
+$conn = mysqli_connect("localhost", "root", "admin", "connect");
+// câu lệnh truy vấn sql
+$sql = "SELECT * FROM db";
+// thực hiện cầu lên truy vấn và kết nối
+$result = mysqli_query($conn, $sql);
+// tạo một mảng
+$arr = array();
+
+while ($rows = mysqli_fetch_assoc($result)) {
+	array_push($arr, $rows);
+}
+// xuất json
+echo json_encode($arr);
+
+?>
