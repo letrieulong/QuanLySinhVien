1bac6680ec2562cd536499ae16b8cdb9fe18d218
letrieulong <letrieulong412000@gmail.com>
2022-04-13 14:54:52 +0700

    c

--- connecttion.php
+++ connecttion.php
@@ -3,9 +3,6 @@ $conn = mysqli_connect("localhost", "root", "admin", "connect") or die("Káº¿t ná
 $sql = "SELECT * FROM db";
 $result = mysqli_query($conn, $sql);
 $arr = array();
-while ($rows = mysqli_fetch_assoc($result)) {
-	array_push($arr, $rows);
-}
 echo "hellomanisdas";
 ?>
 
