2814a1925c8da57f804643db14a84a56f89c9053
letrieulong <letrieulong412000@gmail.com>
2022-04-13 14:51:56 +0700

    da

--- connecttion.php
+++ connecttion.php
@@ -1,22 +1,4 @@
 <?php 
-// kết nối tới mysql
-try {
-$conn = mysqli_connect("localhost", "root", "admin", "connect") or die("Kết nối thất bại");
-// câu lệnh truy vấn sql
-echo "thanh cong";
-$sql = "SELECT * FROM db";
-// thực hiện cầu lên truy vấn và kết nối
-$result = mysqli_query($conn, $sql);
-// tạo một mảng
-$arr = array();
-echo "thanh cong";
-while ($rows = mysqli_fetch_assoc($result)) {
-	array_push($arr, $rows);
-}
-// xuất json
-echo json_encode($arr);
-} catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
-}
+echo "hellomanisdas";
 ?>
 
