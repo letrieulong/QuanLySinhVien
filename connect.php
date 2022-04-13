66e824c185d03807d7396f0ca8fcfa0c94ec6d05
letrieulong <letrieulong412000@gmail.com>
2022-04-13 14:27:38 +0700

    asd

--- connect.php
+++ connect.php
@@ -5,6 +5,22 @@
 	<title>hekllodnasdnasdhjj</title>
 </head>
 <body>
+<?php 
+// kết nối tới mysql
+$conn = mysqli_connect("us-cdbr-east-05.cleardb.net", "be5e9c603b0178", "8bbc452e", "connect");
+// câu lệnh truy vấn sql
+$sql = "SELECT * FROM db";
+// thực hiện cầu lên truy vấn và kết nối
+$result = mysqli_query($conn, $sql);
+// tạo một mảng
+$arr = array();
 
+while ($rows = mysqli_fetch_assoc($result)) {
+	array_push($arr, $rows);
+}
+// xuất json
+echo json_encode($arr);
+
+?>
 </body>
 </html>
