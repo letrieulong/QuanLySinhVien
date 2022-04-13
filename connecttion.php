c4c7a7406274eef737f11771eea4001619585149
letrieulong <letrieulong412000@gmail.com>
2022-04-13 15:10:12 +0700

    fd

--- connecttion.php
+++ connecttion.php
@@ -11,6 +11,7 @@
     while($row =mysqli_fetch_assoc($result))
     {
         array_push($emparray, $row);
+        echo array_push($emparray, $row)."thanh cong";
     }
     echo $emparray."thanh cong";
 
